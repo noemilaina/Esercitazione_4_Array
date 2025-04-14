0d27915594ea313cf6b733f134deeac6a1b20248
noemilaina <s311037@studenti.polito.it>
2025-04-14 19:34:14 +0200

    Update Utils.hpp

--- Utils.hpp
+++ Utils.hpp
@@ -3,12 +3,7 @@
 
 using namespace std;
 
-bool import_data(const string& ifile_path, double& S, size_t& n, double*& w, double*& r);
-
-double tasso_tot(const double& S, const double& V);
-
-double valore_finale(const double& S, const size_t n, const double* const& w, const double* const& r);
-
-string final_result(const double& S, const size_t& n, const double* const& w, const double* const& r, const double& R, const double& V);
-
-bool esporta_data(const string& ofile_path, const string& out_str);
+bool import_data(const char*,filename double& S, int& n, double w[], double r[]);
+void PortfolioFinale(const double w[], const double r[], int n, double S, double& PortfolioReturn, double& V);
+void stampaConsole(double S, int n, const double w[], const double r[], double V, double PortfolioReturn);
+void scriviFile(const char* filename, double S, int n, const double w[], const double r[], double V, double PortfolioReturn);
