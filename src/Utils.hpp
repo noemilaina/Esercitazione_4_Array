c87a9ee6fa612de6a9fd944be07de6541db89b25
noemilaina <s311037@studenti.polito.it>
2025-04-15 14:01:23 +0200

    Update Utils.hpp

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -1,9 +1,10 @@
 #pragma once
 #include <iostream>
 
+
 using namespace std;
 
-bool import_data(const char* filename, double& S, int& n, double w[], double r[]);
-void PortfolioFinale(const double w[], const double r[], int n, double S, double& PortfolioReturn, double& V);
-void stampaConsole(double S, int n, const double w[], const double r[], double V, double PortfolioReturn);
-void scriviFile(const char* filename, double S, int n, const double w[], const double r[], double V, double PortfolioReturn);
+bool import_data(const char* filename, double& S, int& n, double* w, double* r);
+void PortfolioFinale(double* w, double* r, int n, double S, double& PortfolioReturn, double& V);
+void stampaConsole(double S, int n, const double* w, const double* r, double PortfolioReturn, double V);
+void scriviFile(const char* filename, double S, int n, const double* w, const double* r, double PortfolioReturn, double V);
